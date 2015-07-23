0713d66ed1aa52c6992749966e250f5cb2584fff
Klimov Paul <klimov.paul@gmail.com>
2015-07-23 16:45:24 +0300

    Multi part content processing provided

--- Request.php
+++ Request.php
@@ -7,6 +7,7 @@
 
 namespace yii\httpclient;
 use yii\helpers\ArrayHelper;
+use yii\helpers\FileHelper;
 
 /**
  * Request represents HTTP request.
@@ -111,6 +112,71 @@ class Request extends Message
     }
 
     /**
+     * Adds a content part for multi-part content request.
+     * @param string $name part (form input) name.
+     * @param string $content content.
+     * @param array $options content part options, valid options are:
+     *  - contentType - string, part content type
+     *  - fileName - string, name of the uploading file
+     *  - mimeType - string, part content type in case of file uploading
+     * @return $this self reference.
+     */
+    public function addContent($name, $content, $options = [])
+    {
+        $multiPartContent = $this->getContent();
+        if (!is_array($multiPartContent)) {
+            $multiPartContent = [];
+        }
+        $options['content'] = $content;
+        $multiPartContent[$name] = $options;
+        $this->setContent($multiPartContent);
+        return $this;
+    }
+
+    /**
+     * Adds a file for upload as multi-part content.
+     * @see addContent()
+     * @param string $name part (form input) name
+     * @param string $fileName full name of the source file.
+     * @param array $options content part options, valid options are:
+     *  - fileName - string, base name of the uploading file, if not set it base name of the source file will be used.
+     *  - mimeType - string, file mime type, if not set it will be determine automatically from source file.
+     * @return $this
+     */
+    public function addFile($name, $fileName, $options = [])
+    {
+        $content = file_get_contents($fileName);
+        if (!isset($options['mimeType'])) {
+            $options['mimeType'] = FileHelper::getMimeType($fileName);
+        }
+        if (!isset($options['fileName'])) {
+            $options['fileName'] = basename($fileName);
+        }
+        return $this->addContent($name, $content, $options);
+    }
+
+    /**
+     * Adds a string as a file upload.
+     * @see addContent()
+     * @param string $name part (form input) name
+     * @param string $content file content.
+     * @param array $options content part options, valid options are:
+     *  - fileName - string, base name of the uploading file.
+     *  - mimeType - string, file mime type, if not set it 'application/octet-stream' will be used.
+     * @return $this
+     */
+    public function addFileContent($name, $content, $options = [])
+    {
+        if (!isset($options['mimeType'])) {
+            $options['mimeType'] = 'application/octet-stream';
+        }
+        if (!isset($options['fileName'])) {
+            $options['fileName'] = $name . '.dat';
+        }
+        return $this->addContent($name, $content, $options);
+    }
+
+    /**
      * Prepares this request instance for sending.
      * This method should be invoked by transport before sending a request.
      * Do not call this method unless you know what you are doing.
@@ -124,10 +190,91 @@ class Request extends Message
                 $this->setUrl($this->client->baseUrl . '/' . $url);
             }
         }
-        if ($this->getContent() === null) {
+        $content = $this->getContent();
+        if ($content === null) {
             $this->getFormatter()->format($this);
+        } elseif (is_array($content)) {
+            $this->prepareMultiPartContent($content);
         }
         return $this;
+    }
+
+    /**
+     * Prepares multi-part content.
+     * @param array $content multi part content.
+     */
+    private function prepareMultiPartContent(array $content)
+    {
+        static $disallowedChars = ["\0", '"', "\r", "\n"];
+
+        $contentParts = [];
+
+        $data = $this->getData();
+        if (!empty($data)) {
+            foreach ($this->composeFormInputs($data) as $name => $value) {
+                $name = str_replace($disallowedChars, '_', $name);
+                $contentDisposition = 'Content-Disposition: form-data; name="' . $name . '";';
+                $contentParts[] = implode("\r\n", [$contentDisposition, '', $value]);
+            }
+        }
+
+        // process content parts :
+        foreach ($content as $name => $contentParams) {
+            $headers = [];
+            $name = str_replace($disallowedChars, '_', $name);
+            $contentDisposition = 'Content-Disposition: form-data; name="' . $name . '";';
+            if (isset($contentParams['fileName'])) {
+                $fileName = str_replace($disallowedChars, '_', $contentParams['fileName']);
+                $contentDisposition .= ' filename="' . $fileName . '"';
+            }
+            $headers[] = $contentDisposition;
+            if (isset($contentParams['contentType'])) {
+                $headers[] = 'Content-Type: ' . $contentParams['contentType'];
+            } elseif (isset($contentParams['mimeType'])) {
+                $headers[] = 'Content-Type: ' . $contentParams['mimeType'];
+            }
+            $contentParts[] = implode("\r\n", [implode("\r\n", $headers), '', $contentParams['content']]);
+        }
+
+        // generate safe boundary :
+        do {
+            $boundary = "---------------------" . md5(mt_rand() . microtime());
+        } while (preg_grep("/{$boundary}/", $contentParts));
+
+        // add boundary for each part :
+        array_walk($contentParts, function (&$part) use ($boundary) {
+            $part = "--{$boundary}\r\n{$part}";
+        });
+
+        // add final boundary :
+        $contentParts[] = "--{$boundary}--";
+        $contentParts[] = '';
+
+        $this->getHeaders()->set('content-type', "multipart/form-data; boundary={$boundary}");
+        $this->setContent(implode("\r\n", $contentParts));
+    }
+
+    /**
+     * Composes given data as form inputs submitted values, taking in account nested arrays.
+     * Converts `['form' => ['name' => 'value']]` to `['form[name]' => 'value']`.
+     * @param array $data
+     * @param string $baseKey
+     * @return array
+     */
+    private function composeFormInputs(array $data, $baseKey = '')
+    {
+        $result = [];
+        foreach ($data as $key => $value) {
+            if (!empty($baseKey)) {
+                $key = $baseKey . '[' . $key . ']';
+            }
+            if (is_array($value)) {
+                $result = array_merge($result, $this->composeFormInputs($value, $key));
+            } else {
+                $result[$key] = $value;
+            }
+        }
+        return $result;
     }
 
     /**
