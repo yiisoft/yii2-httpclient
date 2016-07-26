729ec854072465523e0d177b2af9fa6db6256227
DrDeath72 <dr.death.ru@gmail.com>
2016-07-26 17:05:23 +0500

    CDATA fix

--- XmlParser.php
+++ XmlParser.php
@@ -33,7 +33,7 @@ class XmlParser extends Object implements ParserInterface
     protected function convertXmlToArray($xml)
     {
         if (!is_object($xml)) {
-            $xml = simplexml_load_string($xml);
+            $xml = simplexml_load_string($xml, 'SimpleXMLElement', LIBXML_NOCDATA);
         }
         $result = (array) $xml;
         foreach ($result as $key => $value) {
