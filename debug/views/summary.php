12a17c62f189eb0fb908307849ec18fb9f253ae7
rhertogh <rhertogh@users.noreply.github.com>
2016-08-11 14:23:22 +0200

    small text fix (#62)
    Debug panel summary text fix

--- debug/views/summary.php
+++ debug/views/summary.php
@@ -5,7 +5,7 @@
 ?>
 <?php if ($queryCount): ?>
 <div class="yii-debug-toolbar__block">
-    <a href="<?= $panel->getUrl() ?>" title="Executed <?= $queryCount ?> database queries which took <?= $queryTime ?>.">
+    <a href="<?= $panel->getUrl() ?>" title="Executed <?= $queryCount ?> HTTP Requests which took <?= $queryTime ?>.">
         HTTP Requests <span class="yii-debug-toolbar__label yii-debug-toolbar__label_info"><?= $queryCount ?></span> <span class="yii-debug-toolbar__label"><?= $queryTime ?></span>
     </a>
 </div>
