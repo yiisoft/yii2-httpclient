f7e04f533e0fc90f127598fdd53743edc99ff6cb
Klimov Paul <klimov.paul@gmail.com>
2017-04-13 12:29:42 +0300

    compatibility with PHPUnit 6.x

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -13,3 +13,5 @@ require_once(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
 Yii::setAlias('@yiiunit/extensions/httpclient', __DIR__);
 Yii::setAlias('@yii/httpclient', dirname(__DIR__));
+
+require_once(__DIR__ . '/compatibility.php');
