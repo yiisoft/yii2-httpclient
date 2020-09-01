a0e6db6df38ce72708887689c465be368fb40172
Oleg Malynovskyi <kwazaro@gmail.com>
2020-09-01 15:41:36 +0300

    FIX: issue 209

--- src/CurlTransport.php
+++ src/CurlTransport.php
@@ -47,7 +47,7 @@ class CurlTransport extends Transport
         curl_close($curlResource);
 
         if ($errorNumber > 0) {
-            throw new Exception('Curl error: #' . $errorNumber . ' - ' . $errorMessage);
+            throw new Exception('Curl error: #' . $errorNumber . ' - ' . $errorMessage, $errorNumber);
         }
 
         $response = $request->client->createResponse($responseContent, $responseHeaders);
